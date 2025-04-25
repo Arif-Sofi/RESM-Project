cd228bca686881e630cfb4bdcf0a7848b4933fed
rrumiyadayo <rrumiyadayo@yahoo.co.jp>
2025-04-25 21:36:09 +0900

    tailwindくっそや

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+      './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+      './storage/framework/views/*.php',
+      './resources/views/**/*.blade.php',
+      './resources/**/*.js',
+      './resources/**/*.vue',
     ],
-
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', 'Noto Sans JP', 'sans-serif'],
-            },
-            // スクリーンフィットのための高さと幅の拡張
-            minHeight: {
-                'screen-fit': '100vh',
-            },
+      extend: {
+        fontFamily: {
+          sans: ['Figtree', 'Noto Sans JP', 'sans-serif'],
         },
+        minHeight: {
+          'screen-fit': '100vh',
+        },
+      },
     },
-
     plugins: [
-        require('@tailwindcss/forms'),
+      require('@tailwindcss/forms'),
     ],
-    // ダークモードを設定（オプション）
-    darkMode: 'class', // または 'media'（システム設定に基づく場合）
-};
+    darkMode: 'class',
+  };
