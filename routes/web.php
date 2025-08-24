0b5455444d56e3fa50a56c4d03b0a75b1c8f8c26
sShi1ro <muhammad.awparif.hakimi@gmail.com>
2025-08-24 11:40:27 +0800

    Redirect '/' to '/login'

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::get('/set-locale/{locale}', [App\Http\Controllers\LocalizationController::class, 'setLocale'])->name('setLocale');
 Route::get('/dashboard', function () {
