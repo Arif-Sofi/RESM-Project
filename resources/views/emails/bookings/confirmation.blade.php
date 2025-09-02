a9d1437a299f63c6fe841cb94d2280efb24adfe3
Azim Sofi <azimmiskandar@gmail.com>
2025-09-02 14:41:04 +0900

    DBのタイムゾーンを表示すること
    戸惑わないように

--- resources/views/emails/bookings/confirmation.blade.php
+++ resources/views/emails/bookings/confirmation.blade.php
@@ -8,8 +8,8 @@ Your booking has been successfully created. Here is a Receipt of your booking.
 - Number of Students: {{ $booking->number_of_student }}
 - Equipment Needed: {{ $booking->equipment_needed }}
 - Purpose: {{ $booking->purpose }}
-- Start Time: {{ $booking->start_time }}
-- End Time: {{ $booking->end_time }}
+- Start Time: {{ $booking->start_time }} ({{ config('app.timezone') }}- Timezone)
+- End Time: {{ $booking->end_time }} ({{ config('app.timezone') }}- Timezone)
 
 Thanks,<br>
 {{ config('app.name') }}
